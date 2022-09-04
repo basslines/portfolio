f4b450ffbeef3d81789dc29079367a24b3522e6b
basslines <admir.osmanovic@gmail.com>
2022-09-05 00:40:12 +0200

    initial commit

---
+++ src/components/Work.jsx
@@ -0,0 +1,96 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import WorkImg from '../assets/workImg.jpeg';
+import movies from '../assets/movies.jpeg';
+import homeImg from '../assets/homeImg.jpeg'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          <p className='py-6'>// Check out some of my recent work</p>
+        </div>
+
+        {/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Github finder App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://github-finder-app-six-chi.vercel.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/basslines/github-finder.git'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${homeImg})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                House Marketplace App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://house-marketplace-app-sand.vercel.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/basslines/house-marketplace-app.git'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${movies})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Movies Search App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://movies-search-opal.vercel.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/basslines/movies-search.git'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
