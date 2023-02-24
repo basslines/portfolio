067b14aab28d35f12f1d52acc0fd1ee501f9308f
admirosmanovic <admirosmanovic@iMac.local>
2023-02-24 12:29:44 +0100

    fix

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,40 +1,38 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import WorkImg from '../assets/workImg.jpeg';
-import movies from '../assets/movies.jpeg';
-import homeImg from '../assets/homeImg.jpeg'
-import tickets from '../assets/tickets.jpeg'
+import WorkImg from "../assets/workImg.jpeg";
+import homeImg from "../assets/homeImg.jpeg";
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
-          <p className='py-6'>// Check out some of my recent work</p>
+          <p className="py-6">// Check out some of my recent work</p>
         </div>
 
         {/* Container */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${WorkImg})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 Github finder App
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://github-finder-app-six-chi.vercel.app/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a href="https://github-finder-app-six-chi.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/basslines/github-finder.git'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                <a href="https://github.com/basslines/github-finder.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -43,21 +41,21 @@ const Work = () => {
           </div>
           <div
             style={{ backgroundImage: `url(${homeImg})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 House Marketplace App
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://house-marketplace-app-sand.vercel.app/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a href="https://house-marketplace-app-sand.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/basslines/house-marketplace-app.git'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                <a href="https://github.com/basslines/house-marketplace-app.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -65,52 +63,6 @@ const Work = () => {
             </div>
           </div>
           {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${movies})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Movies Search App
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='https://movies-search-opal.vercel.app/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='https://github.com/basslines/movies-search.git'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${tickets})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Support Desk App
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='https://admirsupportdesk.herokuapp.com/tickets'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='https://github.com/basslines/support-app.git'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
