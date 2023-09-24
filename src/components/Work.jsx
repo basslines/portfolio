796dffe54fa19f23405a0515ec2b75232e34603d
basslines <admir.osmanovic@gmail.com>
2023-09-24 16:40:29 +0200

    laurenta added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import WorkImg from "../assets/workImg.jpg";
 import homeImg from "../assets/homeImg.jpeg";
+import homeImg1 from "../assets/homeImg1.jpeg";
 
 const Work = () => {
   return (
@@ -16,6 +17,25 @@ const Work = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${homeImg1})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Laurenta Web eShop
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://www.laurentaoliveoil.com/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                
+              </div>
+            </div>
+          </div>
           <div
             style={{ backgroundImage: `url(${WorkImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
